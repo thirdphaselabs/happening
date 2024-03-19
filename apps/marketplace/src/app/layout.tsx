37b542b9f46f02aeb2b997ea1d20ae1510574e1a
Reece Johnson <reece.robert.johnson@gmail.com>
2024-03-19 13:50:06 +0000

    meta

--- apps/marketplace/src/app/layout.tsx
+++ apps/marketplace/src/app/layout.tsx
@@ -4,6 +4,13 @@ import "../styles/input.css";
 import { cookies } from "next/headers";
 import { TRPCReactProvider } from "../trpc/provider";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Plaventi",
+  description: "Event planning, management & ticketing. All in one place.",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
