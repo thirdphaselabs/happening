414eee39120a8c7a24a8051b418c4d789507cef3
Reece Johnson <reece.robert.johnson@gmail.com>
2024-03-29 11:52:15 +0000

    add env var

--- apps/platform/src/app/layout.tsx
+++ apps/platform/src/app/layout.tsx
@@ -10,10 +10,12 @@ import { Toaster } from "~/components/ui/sonner";
 import { TRPCReactProvider } from "../trpc/provider";
 
 import type { Metadata } from "next";
+import { nonNullableEnvironmentVariable } from "~/utils/env";
 
 export const metadata: Metadata = {
   title: "Plaventi",
   description: "Events Re-imagined: Plan, Manage, Sell & Discover",
+  metadataBase: new URL(nonNullableEnvironmentVariable("NEXT_PUBLIC_BASE_URL")),
 };
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-plaventi",});
