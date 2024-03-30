d0471fd3a1381950a1ba95c6ad70755e9584bfad
Reece Johnson <reece.robert.johnson@gmail.com>
2024-03-30 06:38:21 +0000

    feat: remove metadata

--- apps/marketplace/src/app/layout.tsx
+++ apps/marketplace/src/app/layout.tsx
@@ -4,14 +4,12 @@ import "../styles/input.css";
 import { cookies } from "next/headers";
 import { TRPCReactProvider } from "../trpc/provider";
 
-import type { Metadata } from "next";
 import { themeConfig } from "@plaventi/ui";
-import { nonNullableEnvironmentVariable } from "~/utils/env";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Plaventi",
   description: "Event planning, management & ticketing. All in one place.",
-  metadataBase: new URL(nonNullableEnvironmentVariable("NEXT_PUBLIC_BASE_URL")),
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
